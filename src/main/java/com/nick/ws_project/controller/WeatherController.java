958ddd3961dc14c762b294c3e05740505254c081
Nick <nick.ledin@hotmail.com>
2024-09-13 17:26:53 +0200

    add: models of incoming data from api

--- src/main/java/com/nick/ws_project/controller/WeatherController.java
+++ src/main/java/com/nick/ws_project/controller/WeatherController.java
@@ -3,11 +3,7 @@ package com.nick.ws_project.controller;
 import com.nick.ws_project.model.WeatherModel;
 import com.nick.ws_project.service.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -21,15 +17,15 @@ public class WeatherController {
     @Autowired
     public WeatherController(WebClient.Builder weatherWebClient, ApiService apiService) {
         this.weatherWebClient = weatherWebClient
-                .baseUrl("https://api.weatherbit.io/v2.0").build();
+                .baseUrl("https://api.weatherbit.io/v2.0/current").build();
         this.apiService = apiService;
     }
 
-    @GetMapping("/")
-    public Mono<WeatherModel> getCityWeather(@RequestParam String city) {
+    @GetMapping("/{city}")
+    public Mono<WeatherModel> getCityWeather(@PathVariable String city) {
         String apiKey = apiService.getApiKey();
         return weatherWebClient.get()
-                .uri(weather -> weather
+                .uri(UriBuilder -> UriBuilder
                         .queryParam("city", city)
                         .queryParam("key", apiKey)
                         .build())
